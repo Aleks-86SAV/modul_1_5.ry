ceb68d0a9c7a29171605047047aa1d6dd7f5a028
Alexander Smirnov <sanyablask@gmail.com>
2024-12-11 20:25:51 +0300

    Add files via upload
    #immutable_var = 1,2,3,4,5,1.5,3+4
    #print(immutable_var)
    #immutable_var = ('гранат','грейфрукт','ананас')
    #print(immutable_var)
    #immutable_var = (True,False)
    #print(immutable_var)
    #immutable_var = ([1,2,3],['a','b','c'])
    #print(immutable_var)
    #immutable_var = (1,'hello',[1,2,3],{'a':10})
    #print(immutable_var)
    #immutable_var[0] = 150
    #print(immutable_var)
    mutable_list = ([11,12],'z','x','apple')
    print(mutable_list)
    mutable_list [0][0] = 1
    print(mutable_list)
    
    получается:  C:\pythonProjectsForUniversity\LessonO\.venv\Scripts\python.exe "C:\pythonProjectsForUniversity\LessonO\'module_1_5.ry'.py" 
    ([11, 12], 'z', 'x', 'apple')
    ([1, 12], 'z', 'x', 'apple')
    
    Process finished with exit code 0

---
+++ 'module_1_5.ry'.py
@@ -0,0 +1,16 @@
+#immutable_var = 1,2,3,4,5,1.5,3+4
+#print(immutable_var)
+#immutable_var = ('гранат','грейфрукт','ананас')
+#print(immutable_var)
+#immutable_var = (True,False)
+#print(immutable_var)
+#immutable_var = ([1,2,3],['a','b','c'])
+#print(immutable_var)
+#immutable_var = (1,'hello',[1,2,3],{'a':10})
+#print(immutable_var)
+#immutable_var[0] = 150
+#print(immutable_var)
+mutable_list = ([11,12],'z','x','apple')
+print(mutable_list)
+mutable_list [0][0] = 1
+print(mutable_list)
